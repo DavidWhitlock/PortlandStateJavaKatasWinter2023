358e8ac411312b954414e2ef94ddc4c0bca78c4f
David Whitlock <david.m.whitlock@gmail.com>
2023-01-21 10:54:24 -0800

    Added Leap Year Room 3

---
+++ LeapYear/Room3/src/test/java/edu/pdx/cs410J/leapyear/LeapYearRoom3Test.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.leapyear;
+
+import org.junit.jupiter.api.Test;
+
+public class LeapYearRoom3Test
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new LeapYearRoom3();
+  }
+
+}
