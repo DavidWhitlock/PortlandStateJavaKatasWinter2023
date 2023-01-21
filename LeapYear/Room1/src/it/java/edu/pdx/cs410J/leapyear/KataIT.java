35e5b4c10964f47614fe3e97dcb4431af04561ac
David Whitlock <david.m.whitlock@gmail.com>
2023-01-21 10:33:42 -0800

    Added first LeapYear project.

---
+++ LeapYear/Room1/src/it/java/edu/pdx/cs410J/leapyear/KataIT.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.leapyear;
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+class KataIT extends InvokeMainTestCase {
+
+  @Test
+  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+
+}
---
+++ LeapYear/Room1/src/test/java/edu/pdx/cs410J/leapyear/KataTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.leapyear;
+
+import org.junit.jupiter.api.Test;
+
+public class KataTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Kata();
+  }
+
+}
