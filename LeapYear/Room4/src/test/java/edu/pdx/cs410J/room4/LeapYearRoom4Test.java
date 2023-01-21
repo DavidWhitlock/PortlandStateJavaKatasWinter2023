d395fb43db3efb5765cbde80723d7c95e9efba17
David Whitlock <david.m.whitlock@gmail.com>
2023-01-21 11:15:28 -0800

    Room 4

---
+++ LeapYear/Room4/src/test/java/edu/pdx/cs410J/room4/LeapYearRoom4Test.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.room4;
+
+import org.junit.jupiter.api.Test;
+
+public class LeapYearRoom4Test
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new LeapYearRoom4();
+  }
+
+}
