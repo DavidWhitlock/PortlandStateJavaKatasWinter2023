aa4ecf26ddb82e637c365c9dffed51fdc6357f97
rk-pdx <91846253+rk-pdx@users.noreply.github.com>
2023-01-25 20:26:43 -0800

    FInished leap year.

--- LeapYear/Room8/src/main/java/edu/pdx/cs410J/room8/LeapYearRoom8.java
+++ LeapYear/Room8/src/main/java/edu/pdx/cs410J/room8/LeapYearRoom8.java
@@ -6,25 +6,30 @@ public class LeapYearRoom8 {
 
   @VisibleForTesting
   public static void main(String[] args) {
-
-    boolean res = checkForLeapYear(1000);
-    if (res) System.out.println("Leap year!");
-    else {
-      System.out.println("Not a leap year!");
+    if (args.length == 0) {
+      System.err.println("Missing command-line argument for leap year.");
     }
+    else {
+      int num = Integer.parseInt(args[0]);
+      if (checkForLeapYear(num)) {
+        System.out.println("Leap year!");
+      } else {
+        System.out.println("Not a leap year!");
+      }
+    }
+
   }
 
   static boolean checkForLeapYear(int number) {
     boolean val = false;
 
-
     if (number % 400 == 0) {
       val = true;
     }
-    if ((number % 100 == 0) && (number % 400 != 0)) {}
-    if ((number % 4 == 0) && (number % 100 != 0)) {
+    else if ((number % 4 == 0) && (number % 100 != 0)) {
       val = true;
     }
+
     return val;
   }
 }
