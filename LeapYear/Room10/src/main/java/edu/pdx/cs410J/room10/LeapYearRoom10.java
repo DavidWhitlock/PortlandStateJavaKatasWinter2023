8d4c94640907df1d11f303f3625a1529d20cab7c
Tanner <tmestas@pdx.edu>
2023-01-25 20:09:29 -0800

    Added year1700IsNotALeapYear and year2008IsALeapYear tests

--- LeapYear/Room10/src/main/java/edu/pdx/cs410J/room10/LeapYearRoom10.java
+++ LeapYear/Room10/src/main/java/edu/pdx/cs410J/room10/LeapYearRoom10.java
@@ -5,8 +5,15 @@ import com.google.common.annotations.VisibleForTesting;
 public class LeapYearRoom10 {
 
   public static boolean isLeapYear(int year) {
+    if(year%400 == 0){
+      return true;
+    }
+    else if(year%4 == 0 && year%100 != 0){
+      return true;
+    }
     return false;
   }
+
 
   @VisibleForTesting
   public static void main(String[] args) {
