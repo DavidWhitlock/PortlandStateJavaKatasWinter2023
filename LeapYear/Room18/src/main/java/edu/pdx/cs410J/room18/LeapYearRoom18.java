0489fdabf30c2484c123c9b5c7646ba1416f9077
danieltrang <daniel.trang@act-on.com>
2023-01-25 20:57:10 -0800

    David Baker-Robinson & Daniel Trang, final Room 18 submission

--- LeapYear/Room18/src/main/java/edu/pdx/cs410J/room18/LeapYearRoom18.java
+++ LeapYear/Room18/src/main/java/edu/pdx/cs410J/room18/LeapYearRoom18.java
@@ -4,22 +4,38 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class LeapYearRoom18 {
   public static class LeapYearLogic {
-    public boolean yearDivisibleBy400IsALeapYear(int year) {
-      return true;
+
+    /**
+     * If year is divisible by 400, then it is a Leap Year
+     * @param year
+     * @return
+     */
+    static boolean yearDivisibleBy400IsALeapYear(int year) {
+      return (year % 400 == 0);
     }
 
-    public static boolean yearDivisibleBy100ButNot400IsALeapYear(int year) {
-      return true;
+    /**
+     * If year is divisible by 100, but not 400 then it is a Leap Year
+     * @param year
+     * @return
+     */
+    public static boolean yearDivisibleBy100ButNot400IsNotLeapYear(int year) {
+      return (year % 100 == 0 && year % 400 != 0);
     }
 
-    public static boolean yearDivisibleBy4ButNot100IsALeapYear(int year) {
-      return true;
+    /**
+     * <p>If years is divisible by 4, but not 100 then it is a Leap Year</p>
+     * @param year
+     * @return Return true if year is a Leap Year, returns false if year is not a Leap Year
+     */
+    static boolean yearDivisibleBy4ButNot100IsALeapYear(int year) {
+      return (year % 4 == 0 && year % 100 != 0);
     }
 
     /**
      * <p>Leap years must be divisible by 4</p>
      * @param year
-     * @return
+     * @return Return true if year is a Leap Year, returns false if year is not a Leap Year
      */
     static boolean LeapYearMustBeDivisibleBy4(int year) {
       return (year % 4 == 0);
@@ -27,6 +43,25 @@ public class LeapYearRoom18 {
   }
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    int year=0;
+    if(args.length == 0){
+      System.err.println("Missing command line arguments");
+    }
+
+    //catch if input is not a valid number
+    try{
+      year = Integer.parseInt(args[0]);
+    } catch (Exception err) {
+      System.err.println("ERROR: Invalid argument input. Must be a valid integer.");
+      return;
+    }
+
+    if(LeapYearLogic.yearDivisibleBy400IsALeapYear(year)
+            || LeapYearLogic.yearDivisibleBy4ButNot100IsALeapYear(year)){
+              System.out.println("Input year is a Leap Year, huzzah!");
+    }
+    else{
+      System.out.println("Not a Leap Year. Sorry bud. *sad trombone sounds*");
+    }
   }
 }
--- LeapYear/Room18/src/test/java/edu/pdx/cs410J/room18/LeapYearRoom18Test.java
+++ LeapYear/Room18/src/test/java/edu/pdx/cs410J/room18/LeapYearRoom18Test.java
@@ -21,4 +21,28 @@ public class LeapYearRoom18Test
     assertThat(LeapYearRoom18.LeapYearLogic.LeapYearMustBeDivisibleBy4(notLeapYear), is(false));
     assertThat(LeapYearRoom18.LeapYearLogic.LeapYearMustBeDivisibleBy4(LeapYear), is(true));
   }
+
+  @Test
+  void YearIsDivisibleBy4ButNotBy100() {
+    int notLeapYear = 2400;
+    int LeapYear = 2008;
+    assertThat(LeapYearRoom18.LeapYearLogic.yearDivisibleBy4ButNot100IsALeapYear(notLeapYear), is(false));
+    assertThat(LeapYearRoom18.LeapYearLogic.yearDivisibleBy4ButNot100IsALeapYear(LeapYear), is(true));
+  }
+
+  @Test
+  void YearIsDivisibleBy100ButNotBy400() {
+    int notLeapYear = 2400;
+    int LeapYear = 2100;
+    assertThat(LeapYearRoom18.LeapYearLogic.yearDivisibleBy100ButNot400IsNotLeapYear(notLeapYear), is(false));
+    assertThat(LeapYearRoom18.LeapYearLogic.yearDivisibleBy100ButNot400IsNotLeapYear(LeapYear), is(true));
+  }
+
+  @Test
+  void YearDivisibleBy400IsLeapYear() {
+    int notLeapYear = 1999;
+    int LeapYear = 2000;
+    assertThat(LeapYearRoom18.LeapYearLogic.yearDivisibleBy400IsALeapYear(notLeapYear), is(false));
+    assertThat(LeapYearRoom18.LeapYearLogic.yearDivisibleBy400IsALeapYear(LeapYear), is(true));
+  }
 }
