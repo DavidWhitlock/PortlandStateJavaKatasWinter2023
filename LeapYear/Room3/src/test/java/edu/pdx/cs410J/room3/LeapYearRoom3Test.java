dfaf9b02b5ff07b4cf49bdfe7318eb12d8fe04a8
Tim <halltim@pdx.edu>
2023-01-25 21:18:18 -0800

    Room 3 commit

--- LeapYear/Room3/src/test/java/edu/pdx/cs410J/room3/LeapYearRoom3Test.java
+++ LeapYear/Room3/src/test/java/edu/pdx/cs410J/room3/LeapYearRoom3Test.java
@@ -2,12 +2,22 @@ package edu.pdx.cs410J.room3;
 
 import org.junit.jupiter.api.Test;
 
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class LeapYearRoom3Test
 {
-
+/**
+ * Validate the year
+ */
   @Test
-  void canInstantiateKataClass() {
-    new LeapYearRoom3();
+  void validateYear(){
+    int invalidYear = -134;
+    int validYear = 2023;
+
+    assertThat(LeapYearRoom3.isValidYear(invalidYear),equalTo(false));
+    assertThat(LeapYearRoom3.isValidYear(validYear),equalTo(false));
   }
 
 }
