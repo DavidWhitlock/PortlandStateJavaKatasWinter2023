646d339ecf5f5c3cc340b5df7bc9b585751c29aa
Clara Stickney <cstick2@pdx.edu>
2023-01-25 18:22:58 -0800

    fizzbuzz first test progress

--- FizzBuzz/src/main/java/edu/pdx/cs410J/pairing/FizzBuzz.java
+++ FizzBuzz/src/main/java/edu/pdx/cs410J/pairing/FizzBuzz.java
@@ -4,6 +4,9 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class FizzBuzz {
 
+  public static String fizzBuzz(int number){
+    return "blah";
+  }
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
--- FizzBuzz/src/test/java/edu/pdx/cs410J/pairing/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/edu/pdx/cs410J/pairing/FizzBuzzTest.java
@@ -2,12 +2,14 @@ package edu.pdx.cs410J.pairing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class FizzBuzzTest
 {
-
   @Test
-  void canInstantiateFizzBuzzClass() {
-    new FizzBuzz();
+  void theWordThreeIsFizz(){
+    assertThat(FizzBuzz.fizzBuzz(3), equalTo("fizz"));
   }
 
 }
