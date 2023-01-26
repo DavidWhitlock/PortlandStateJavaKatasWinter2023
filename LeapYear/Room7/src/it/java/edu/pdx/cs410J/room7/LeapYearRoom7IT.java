51025861f13fe5079ce4444656a4c4463aca9046
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2023-01-26 10:06:02 +0530

    introduced constants for messages

--- LeapYear/Room7/src/it/java/edu/pdx/cs410J/room7/LeapYearRoom7IT.java
+++ LeapYear/Room7/src/it/java/edu/pdx/cs410J/room7/LeapYearRoom7IT.java
@@ -3,30 +3,32 @@ package edu.pdx.cs410J.room7;
 import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.jupiter.api.Test;
 
+import static edu.pdx.cs410J.room7.LeapYearRoom7.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
 
 class LeapYearRoom7IT extends InvokeMainTestCase {
 
   private MainMethodResult invokeMain(String... args) {
-    return invokeMain( LeapYearRoom7.class, args );
+    return invokeMain(LeapYearRoom7.class, args);
   }
+
   @Test
   void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
     InvokeMainTestCase.MainMethodResult result = invokeMain();
-    assertThat(result.getTextWrittenToStandardError(), containsString("Missing arguments"));
+    assertThat(result.getTextWrittenToStandardError(), containsString(MISSING_ARGUMENTS));
   }
 
   @Test
   void checkNonNumericIsLeapYear() {
     InvokeMainTestCase.MainMethodResult result = invokeMain("abcd");
-    assertThat(result.getTextWrittenToStandardError(), containsString("Invalid input year provided. Please enter numeric value."));
+    assertThat(result.getTextWrittenToStandardError(), containsString(INVALID_INPUT_YEAR));
   }
 
   @Test
   void checkMultipleArgumentsReturnsError() {
     InvokeMainTestCase.MainMethodResult result = invokeMain("2012", "2000");
-    assertThat(result.getTextWrittenToStandardError(), containsString("Too many arguments. Please enter a single numeric value."));
+    assertThat(result.getTextWrittenToStandardError(), containsString(TOO_MANY_ARGUMENTS));
   }
 
   @Test
