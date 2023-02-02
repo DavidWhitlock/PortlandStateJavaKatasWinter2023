7c04ac4dcbae55ce0422ec46bad7734e4fc1f31c
David Whitlock <david.m.whitlock@gmail.com>
2023-02-01 19:50:47 -0800

    Implemented the Diamond.

--- Diamond/DiamondRoom13/src/main/java/edu/pdx/cs410J/room13/DiamondRoom13.java
+++ Diamond/DiamondRoom13/src/main/java/edu/pdx/cs410J/room13/DiamondRoom13.java
@@ -9,7 +9,31 @@ public class DiamondRoom13 {
     System.err.println("Missing command line arguments");
   }
 
-  public static String diamond(char c) {
-    return "A";
+  public static String diamond(char middleChar) {
+    StringBuilder sb = new StringBuilder();
+    for (char c = 'A'; c <= middleChar; c++) {
+      row(middleChar, sb, c);
+    }
+
+    for (char c = (char) (middleChar - 1); c >= 'A'; c--) {
+      row(middleChar, sb, c);
+    }
+
+    return sb.toString();
   }
+
+  private static void row(char middleChar, StringBuilder sb, char c) {
+    int indent = middleChar - c;
+    sb.append(" ".repeat(indent));
+
+    int space = c - 'A';
+    if (space > 0) {
+      sb.append(c);
+      sb.append(" ".repeat((2 * space) - 1));
+    }
+
+    sb.append(c);
+    sb.append('\n');
+  }
+
 }
--- Diamond/DiamondRoom13/src/test/java/edu/pdx/cs410J/room13/DiamondRoom13Test.java
+++ Diamond/DiamondRoom13/src/test/java/edu/pdx/cs410J/room13/DiamondRoom13Test.java
@@ -11,8 +11,58 @@ public class DiamondRoom13Test
   @Test
   void a() {
     String diamond =
-      "A";
+      "A\n";
     assertThat(DiamondRoom13.diamond('A'), equalTo(diamond));
+  }
+  @Test
+  void b() {
+    String diamond =
+      " A\n" +
+      "B B\n" +
+      " A\n";
+    assertThat(DiamondRoom13.diamond('B'), equalTo(diamond));
+  }
+
+  @Test
+  void c() {
+    String diamond =
+      "  A\n" +
+      " B B\n" +
+      "C   C\n" +
+      " B B\n" +
+      "  A\n" +
+        "";
+    assertThat(DiamondRoom13.diamond('C'), equalTo(diamond));
+  }
+
+  @Test
+  void d() {
+    String diamond =
+      "   A\n" +
+      "  B B\n" +
+      " C   C\n" +
+      "D     D\n" +
+      " C   C\n" +
+      "  B B\n" +
+      "   A\n" +
+        "";
+    assertThat(DiamondRoom13.diamond('D'), equalTo(diamond));
+  }
+
+  @Test
+  void e() {
+    String diamond =
+      "    A\n" +
+      "   B B\n" +
+      "  C   C\n" +
+      " D     D\n" +
+      "E       E\n" +
+      " D     D\n" +
+      "  C   C\n" +
+      "   B B\n" +
+      "    A\n" +
+      "";
+    assertThat(DiamondRoom13.diamond('E'), equalTo(diamond));
   }
 
 }
