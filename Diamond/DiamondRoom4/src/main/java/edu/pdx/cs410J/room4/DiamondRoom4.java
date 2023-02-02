c3071ecfd2b4b6a6db6dfe6f669d5839af099e1b
John Lorenz IV <lorenz2@pdx.edu>
2023-02-01 19:39:47 -0800

    Init room4

--- Diamond/DiamondRoom4/src/main/java/edu/pdx/cs410J/room4/DiamondRoom4.java
+++ Diamond/DiamondRoom4/src/main/java/edu/pdx/cs410J/room4/DiamondRoom4.java
@@ -1,9 +1,10 @@
 package edu.pdx.cs410J.room4;
 
+import java.util.HashMap;
 import com.google.common.annotations.VisibleForTesting;
 
 public class DiamondRoom4 {
-
+  public String[] lettersInOrder = {"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"};
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
--- Diamond/DiamondRoom4/src/test/java/edu/pdx/cs410J/room4/DiamondRoom4Test.java
+++ Diamond/DiamondRoom4/src/test/java/edu/pdx/cs410J/room4/DiamondRoom4Test.java
@@ -1,13 +1,27 @@
 package edu.pdx.cs410J.room4;
 
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class DiamondRoom4Test
 {
 
   @Test
   void canInstantiateKataClass() {
+
     new DiamondRoom4();
+  }
+  @Test
+  void lettersInOrderContainsOnlyCharacters(){
+    DiamondRoom4 dr4 = new DiamondRoom4();
+    for (String c : dr4.lettersInOrder){
+      assertThat(c.length(), equalTo(1));
+    }
+  }
+  @Test
+  void onlyOneSpacePerCharacter(){
+
   }
 
 }
