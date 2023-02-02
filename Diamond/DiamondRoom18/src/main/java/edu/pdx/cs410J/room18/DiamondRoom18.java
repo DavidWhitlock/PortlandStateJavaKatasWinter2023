781d17a7cb4cc78c8e6d4c2679eeb9f3d1bb3aec
danieltrang <daniel.trang@act-on.com>
2023-02-01 19:42:07 -0800

    room 18 changes

--- Diamond/DiamondRoom18/src/main/java/edu/pdx/cs410J/room18/DiamondRoom18.java
+++ Diamond/DiamondRoom18/src/main/java/edu/pdx/cs410J/room18/DiamondRoom18.java
@@ -6,6 +6,15 @@ public class DiamondRoom18 {
 
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    final String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    final int numberA = (int) 'A';
+
+    //line width is (N * 2 - 1) where N-index = position of letter
+    //input is Character
+    int numberUserInput = (int) args[0].toUpperCase().charAt(0);
+    //subtract input to A to get N-index
+    int letterIndex = numberUserInput - numberA;
+
+    System.out.println(letterIndex);
   }
 }
--- Diamond/DiamondRoom18/src/test/java/edu/pdx/cs410J/room18/DiamondRoom18Test.java
+++ Diamond/DiamondRoom18/src/test/java/edu/pdx/cs410J/room18/DiamondRoom18Test.java
@@ -2,12 +2,18 @@ package edu.pdx.cs410J.room18;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class DiamondRoom18Test
 {
 
   @Test
-  void canInstantiateKataClass() {
-    new DiamondRoom18();
+  void testCorrectNumberLetterReturn() {
+    int numberUserInput = (int) 'K';
+    int numberA = (int) 'A';
+    int result = numberUserInput - numberA;
+    assertThat(result, equalTo(10));
   }
 
 }
