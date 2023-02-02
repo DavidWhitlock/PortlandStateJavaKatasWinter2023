a3c95bedd4e45f83e8932c81e5ea547c7825f49d
Logan Anderson <log9@pdx.edu>
2023-02-01 19:50:05 -0800

    A case added to DiamondRoom20 and test

--- Diamond/DiamondRoom20/src/main/java/edu/pdx/cs410J/room20/DiamondRoom20.java
+++ Diamond/DiamondRoom20/src/main/java/edu/pdx/cs410J/room20/DiamondRoom20.java
@@ -4,8 +4,13 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class DiamondRoom20 {
 
+  public String drawDiamond(char letter){
+    return "" + letter;
+  }
+
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+
+
   }
 }
