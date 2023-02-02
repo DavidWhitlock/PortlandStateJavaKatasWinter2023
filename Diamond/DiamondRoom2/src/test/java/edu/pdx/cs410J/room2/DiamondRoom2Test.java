fd2518c32a7c6dbf8dd278125d4a47455d145950
kgujral2612 <kgujral@pdx.edu>
2023-02-01 19:38:19 -0800

    Initial commit for Room2

--- Diamond/DiamondRoom2/src/test/java/edu/pdx/cs410J/room2/DiamondRoom2Test.java
+++ Diamond/DiamondRoom2/src/test/java/edu/pdx/cs410J/room2/DiamondRoom2Test.java
@@ -1,13 +1,18 @@
 package edu.pdx.cs410J.room2;
-
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class DiamondRoom2Test
 {
+  @Test
+  void printsTheCorrectPatternForA(){
+    assertThat(DiamondRoom2.createDiamond('A'), equalTo("A"));
+  }
 
   @Test
-  void canInstantiateKataClass() {
-    new DiamondRoom2();
+  void printsTheCorrectPatternForB(){
+    assertThat(DiamondRoom2.createDiamond('B'), equalTo(" A \nB  B\n A "));
   }
 
 }
