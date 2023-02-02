6ee58eb20e5857673e6b98bc5642adbd05f135e5
devonfox <devonhouston.fox@gmail.com>
2023-02-01 18:15:04 -0800

    add romanNumeral I test

--- RomanNumerals/src/main/java/edu/pdx/cs410J/pairing/RomanNumerals.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/pairing/RomanNumerals.java
@@ -6,6 +6,11 @@ public class RomanNumerals {
 
   @VisibleForTesting
   public static void main(String[] args) {
+
     System.err.println("Missing command line arguments");
+  }
+
+  public static String romanNumeral(int number) {
+    return "I";
   }
 }
--- RomanNumerals/src/test/java/edu/pdx/cs410J/pairing/RomanNumeralsTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/pairing/RomanNumeralsTest.java
@@ -2,12 +2,18 @@ package edu.pdx.cs410J.pairing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class RomanNumeralsTest
 {
 
   @Test
-  void canInstantiateFizzBuzzClass() {
-    new RomanNumerals();
+  void romanNumberOneisI() {
+    String result = RomanNumerals.romanNumeral(1);
+    assertThat(result, equalTo("I"));
   }
+
+
 
 }
