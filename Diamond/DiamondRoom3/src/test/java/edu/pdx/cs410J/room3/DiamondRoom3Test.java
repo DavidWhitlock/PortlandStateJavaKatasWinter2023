57d3a2bf7310f64ee2c843ebdc4b5ade5cf8d2e6
Matthew Cole <matthewcole66@gmail.com>
2023-02-01 19:38:31 -0800

    DiamondRoom3 Pair programming

--- Diamond/DiamondRoom3/src/test/java/edu/pdx/cs410J/room3/DiamondRoom3Test.java
+++ Diamond/DiamondRoom3/src/test/java/edu/pdx/cs410J/room3/DiamondRoom3Test.java
@@ -2,12 +2,55 @@ package edu.pdx.cs410J.room3;
 
 import org.junit.jupiter.api.Test;
 
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class DiamondRoom3Test
 {
 
+ // @Test
+  //void canInstantiateKataClass() {
+    //new DiamondRoom3();
+ // }
   @Test
-  void canInstantiateKataClass() {
-    new DiamondRoom3();
+  void testToNumberC()
+  {
+    assertThat(DiamondRoom3.ToNumber('C'), equalTo(3));
   }
-
+  @Test
+  void testToNumberF()
+  {
+    assertThat(DiamondRoom3.ToNumber('F'), equalTo(6));
+  }
+  @Test
+  void testToNumberG()
+  {
+    assertThat(DiamondRoom3.ToNumber('G'), equalTo(7));
+  }
+  @Test
+  void testToNumberA()
+  {
+    assertThat(DiamondRoom3.ToNumber('A'), equalTo(1));
+  }
+  @Test
+  void testToChar3()
+  {
+    assertThat(DiamondRoom3.ToChar(3), equalTo('C'));
+  }
+  @Test
+  void testToChar7()
+  {
+    assertThat(DiamondRoom3.ToChar(7), equalTo('G'));
+  }
+  @Test
+  void testToChar6()
+  {
+    assertThat(DiamondRoom3.ToChar(6), equalTo('F'));
+  }
+  @Test
+  void testToChar1()
+  {
+    assertThat(DiamondRoom3.ToChar(1), equalTo('A'));
+  }
 }
