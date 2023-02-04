722e2f8b363d597106f7abb74bbdb98db9dd105a
David Whitlock <david.m.whitlock@gmail.com>
2023-02-04 06:15:54 -0800

    First room for the Lags kata

---
+++ Lags/LagsRoom1/src/it/java/edu/pdx/cs410J/room1/LagsRoom1IT.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.room1;
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+class LagsRoom1IT extends InvokeMainTestCase {
+
+  @Test
+  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LagsRoom1.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+
+}
---
+++ Lags/LagsRoom1/src/test/java/edu/pdx/cs410J/room1/LagsRoom1Test.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.room1;
+
+import org.junit.jupiter.api.Test;
+
+public class LagsRoom1Test
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new LagsRoom1();
+  }
+
+}
