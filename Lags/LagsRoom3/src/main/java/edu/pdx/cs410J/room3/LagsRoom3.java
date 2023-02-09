1e7bb296cc387c58ab6cce41ca283937350818de
boalo <c.chlin@icloud.com>
2023-02-08 20:32:34 -0800

    room 3

--- Lags/LagsRoom3/src/main/java/edu/pdx/cs410J/room3/LagsRoom3.java
+++ Lags/LagsRoom3/src/main/java/edu/pdx/cs410J/room3/LagsRoom3.java
@@ -2,6 +2,8 @@ package edu.pdx.cs410J.room3;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.ArrayList;
+
 public class LagsRoom3 {
 
   @VisibleForTesting
@@ -9,17 +11,51 @@ public class LagsRoom3 {
     System.err.println("Missing command line arguments");
   }
 
+//  public static boolean valid(Flight flight1, Flight flight2) {
+//  }
+
   @VisibleForTesting
-  public class Flight {
+  public static class Flight {
     public int startTime;
     public int duration;
     public int price;
+    public float ratio;
+
 
     public Flight(int startTime, int duration, int price) {
       this.startTime = startTime;
       this.duration =duration;
       this.price = price;
+      this.ratio = (float)price/duration;
     }
+    
+    public static boolean valid(Flight flight1, Flight flight2) {
+      if ((flight1.startTime+flight1.duration) > (flight2.startTime)) {
+        return false;
+      }
+      return true;
+    }
+
+    public static void bestCombo(ArrayList<Flight> flights1) {
+      ArrayList<Flight> flights2 = flights1;
+
+      int counter = 0;
+      for (int i = 0; i < flights1.size(); i++) {
+        for (int j = 0; j < flights1.size(); j++) {
+          if (i != j) {
+            if (valid(flights1.get(i), flights2.get(j))) {
+              if (flights1.get(i).ratio >= flights2.get(j).ratio)  {
+                counter += flights1.get(i).price;
+              }
+              else {
+                counter += flights2.get(j).price;
+              }
+            }
+          }
+        }
+      }
+    }
+
   }
 
 }
--- Lags/LagsRoom3/src/test/java/edu/pdx/cs410J/room3/LagsRoom3Test.java
+++ Lags/LagsRoom3/src/test/java/edu/pdx/cs410J/room3/LagsRoom3Test.java
@@ -4,9 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LagsRoom3Test
 {
+  LagsRoom3.Flight flight1 = new LagsRoom3.Flight(0,5,10);
+  LagsRoom3.Flight flight2 = new LagsRoom3.Flight(3,7,14);
+  LagsRoom3.Flight flight3 = new LagsRoom3.Flight(5,9,7);
+  LagsRoom3.Flight flight4 = new LagsRoom3.Flight(6,9,8);
+
 
   @Test
   void canInstantiateKataClass() {
@@ -15,8 +22,43 @@ public class LagsRoom3Test
 
   @Test
   void testFlightStartTime() {
-    Flight flight = new Flight(0, 5, 10);
-    assertThat(0, equalTo(flght.startTime));
+    LagsRoom3.Flight flight = new LagsRoom3.Flight(0, 5, 10);
+    assertThat(0, equalTo(flight.startTime));
+  }
+
+
+  @Test
+  void testFlgithtDuration() {
+    LagsRoom3.Flight flight = new LagsRoom3.Flight(0,5,10);
+    assertThat(5, equalTo(flight.duration));
+  }
+  @Test
+  void testFlgithtPrice() {
+    LagsRoom3.Flight flight = new LagsRoom3.Flight(0,5,10);
+    assertThat(10, equalTo(flight.price));
+  }
+
+  @Test
+  void testInvalidStartingTime() {
+    LagsRoom3.Flight flight1 = new LagsRoom3.Flight(0,5,10);
+
+    LagsRoom3.Flight flight2 = new LagsRoom3.Flight(3,7,14);
+
+    assertFalse(LagsRoom3.Flight.valid(flight1, flight2));
+  }
+
+  @Test
+  void testValidStartTime() {
+    LagsRoom3.Flight flight1 = new LagsRoom3.Flight(0,5,10);
+
+    LagsRoom3.Flight flight2 = new LagsRoom3.Flight(5,7,14);
+
+    assertTrue(LagsRoom3.Flight.valid(flight1,flight2));
+
+  }
+  @Test
+  void testPriceRatio() {
+    assertThat(flight1.ratio, equalTo(flight2.ratio));
   }
 
 }
