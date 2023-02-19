13cf947562d4cd3599a7464f3a8fa6418b2ab793
David Whitlock <david.m.whitlock@gmail.com>
2023-02-18 17:21:26 -0800

    Add project for the Tennis kata.

---
+++ Tennis/src/it/java/edu/pdx/cs410J/mobbing/TennisIT.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.mobbing;
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+class TennisIT extends InvokeMainTestCase {
+
+  @Test
+  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Tennis.class);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+
+}
