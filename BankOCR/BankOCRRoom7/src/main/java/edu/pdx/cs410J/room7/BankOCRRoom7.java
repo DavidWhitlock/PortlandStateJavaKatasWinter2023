297f5e3ac1ae4b0043e35fbe2c7fe97b16d615d5
Leo Lu <leolu1706@gmail.com>
2023-03-01 20:50:35 -0800

    Leo driver test all numbers

--- BankOCR/BankOCRRoom7/src/main/java/edu/pdx/cs410J/room7/BankOCRRoom7.java
+++ BankOCR/BankOCRRoom7/src/main/java/edu/pdx/cs410J/room7/BankOCRRoom7.java
@@ -3,7 +3,16 @@ package edu.pdx.cs410J.room7;
 import com.google.common.annotations.VisibleForTesting;
 
 public class BankOCRRoom7 {
-
+  final String ZERO = " _ | ||_|";
+  final String ONE = "     |  |";
+  final String TWO = " _  _||_ ";
+  final String THREE = " _  _| _|";
+  final String FOUR = "   |_|  |";
+  final String FIVE = " _ |_  _|";
+  final String SIX = " _ |_ |_|";
+  final String SEVEN = " _   |  |";
+  final String EIGHT = " _ |_||_|";
+  final String NINE = " _ |_| _|";
 
 
   @VisibleForTesting
@@ -21,4 +30,6 @@ public class BankOCRRoom7 {
 
     return stringyness;
   }
+
+
 }
--- BankOCR/BankOCRRoom7/src/test/java/edu/pdx/cs410J/room7/BankOCRRoom7Test.java
+++ BankOCR/BankOCRRoom7/src/test/java/edu/pdx/cs410J/room7/BankOCRRoom7Test.java
@@ -23,4 +23,29 @@ public class BankOCRRoom7Test
     assertThat(room.validateString(first), equalTo(27));
   }
 
+  @Test
+  void testAllNumbers(){
+    BankOCRRoom7 room = new BankOCRRoom7();
+    String zero = " _ | ||_|";
+    assertThat(room.ZERO,equalTo(zero));
+    String one = "     |  |";
+    assertThat(room.ONE,equalTo(one));
+    String two = " _  _||_ ";
+    assertThat(room.TWO,equalTo(two));
+    String three = " _  _| _|";
+    assertThat(room.THREE,equalTo(three));
+    String four = "   |_|  |";
+    assertThat(room.FOUR,equalTo(four));
+    String five = " _ |_  _|";
+    assertThat(room.FIVE,equalTo(five));
+    String six = " _ |_ |_|";
+    assertThat(room.SIX,equalTo(six));
+    String seven = " _   |  |";
+    assertThat(room.SEVEN,equalTo(seven));
+    String eight = " _ |_||_|";
+    assertThat(room.EIGHT,equalTo(eight));
+    String nine = " _ |_| _|";
+    assertThat(room.NINE,equalTo(nine));
+  }
+
 }
