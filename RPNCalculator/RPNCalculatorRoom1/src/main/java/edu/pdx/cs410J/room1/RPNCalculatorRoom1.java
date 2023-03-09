0c5cde9971b2861113b924ded1c6910076560c33
David <davvan@pdx.edu>
2023-03-08 19:37:24 -0800

    Pushed by David Van

--- RPNCalculator/RPNCalculatorRoom1/src/main/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1.java
+++ RPNCalculator/RPNCalculatorRoom1/src/main/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1.java
@@ -4,6 +4,11 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class RPNCalculatorRoom1 {
 
+  public int calculateRPN(String expression) {
+    return 0;
+  }
+
+
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
--- RPNCalculator/RPNCalculatorRoom1/src/test/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1Test.java
+++ RPNCalculator/RPNCalculatorRoom1/src/test/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1Test.java
@@ -2,12 +2,22 @@ package edu.pdx.cs410J.room1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class RPNCalculatorRoom1Test
 {
 
   @Test
   void canInstantiateKataClass() {
     new RPNCalculatorRoom1();
+  }
+
+  @Test
+  void canAddTwoNumbers() {
+    String args = "2 4 +";
+    RPNCalculatorRoom1 rpn = new RPNCalculatorRoom1();
+    assertThat(rpn.calculateRPN(args), equalTo(6));
   }
 
 }
