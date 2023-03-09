c33581fd7b0726818caf41cb8ecf5b28bd278a5c
SHREYOSHI MAHATO <shreyoshimahato@gmail.com>
2023-03-09 10:09:35 +0530

    max and sqrt

--- RPNCalculator/RPNCalculatorRoom1/src/main/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1.java
+++ RPNCalculator/RPNCalculatorRoom1/src/main/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1.java
@@ -11,14 +11,14 @@ public class RPNCalculatorRoom1 {
   public int calculateRPN(String expression) {
 
     //parse the expression
-    String []  ops = expression.split(" ");
+    String[] ops = expression.split(" ");
 
-    Stack <Integer> stack  = new Stack<>();
+    Stack<Integer> stack = new Stack<>();
     int result = 0;
     int first;
     int second;
-    for(String op : ops) {
-      switch(op) {
+    for (String op : ops) {
+      switch (op) {
         case "+":
           second = stack.pop();
           first = stack.pop();
@@ -46,11 +46,28 @@ public class RPNCalculatorRoom1 {
           result = first * second;
           stack.push(result);
           break;
-
+        case "SQRT":
+          first = stack.pop();
+          result = (int) Math.sqrt(first);
+          stack.push(result);
+          break;
+        case "MAX":
+          result = stack.pop();
+          while (!stack.empty()) {
+            if (result < stack.peek())
+              result = stack.pop();
+            else
+              stack.pop();
+          }
+          stack.push(result);
+          break;
         default:
           try {
             stack.add(Integer.parseInt(op));
-          } catch (NumberFormatException e) {System.err.println(e.getMessage()); break;}
+          } catch (NumberFormatException e) {
+            System.err.println(e.getMessage());
+            break;
+          }
 
       } // end of switch
 
--- RPNCalculator/RPNCalculatorRoom1/src/test/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1Test.java
+++ RPNCalculator/RPNCalculatorRoom1/src/test/java/edu/pdx/cs410J/room1/RPNCalculatorRoom1Test.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class RPNCalculatorRoom1Test
-{
+public class RPNCalculatorRoom1Test {
 
   @Test
   void canInstantiateKataClass() {
@@ -21,7 +20,7 @@ public class RPNCalculatorRoom1Test
   }
 
   @Test
-  void canAddTwoNumbersAndThenSubtractAThirdNumber(){
+  void canAddTwoNumbersAndThenSubtractAThirdNumber() {
     String args = "4 2 + 3 - ";
     RPNCalculatorRoom1 rpn = new RPNCalculatorRoom1();
     assertThat(rpn.calculateRPN(args), equalTo(3));
@@ -39,6 +38,27 @@ public class RPNCalculatorRoom1Test
     String args = "20 5 /";
     RPNCalculatorRoom1 rpn = new RPNCalculatorRoom1();
     assertThat(rpn.calculateRPN(args), equalTo(4));
+  }
+
+  @Test
+  void Sqrt() {
+    String args = "9 SQRT";
+    RPNCalculatorRoom1 rpn = new RPNCalculatorRoom1();
+    assertThat(rpn.calculateRPN(args), equalTo(3));
+  }
+
+  @Test
+  void Max() {
+    String args = "5 3 4 2 9 1 MAX";
+    RPNCalculatorRoom1 rpn = new RPNCalculatorRoom1();
+    assertThat(rpn.calculateRPN(args), equalTo(9));
+  }
+
+  @Test
+  void MaxPlus() {
+    String args = "4 5 MAX 1 +";
+    RPNCalculatorRoom1 rpn = new RPNCalculatorRoom1();
+    assertThat(rpn.calculateRPN(args), equalTo(6));
   }
 
 }
