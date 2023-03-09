13c52bf35953e582634187b8ed6db3c579ac7967
Logan Anderson <log9@pdx.edu>
2023-03-08 20:42:54 -0800

    Fixed tests

--- RPNCalculator/RPNCalculatorRoom7/src/test/java/edu/pdx/cs410J/room7/RPNCalculatorRoom7Test.java
+++ RPNCalculator/RPNCalculatorRoom7/src/test/java/edu/pdx/cs410J/room7/RPNCalculatorRoom7Test.java
@@ -1,8 +1,10 @@
 package edu.pdx.cs410J.room7;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -23,43 +25,51 @@ public class RPNCalculatorRoom7Test
 
   @Test
   void testCalculateRPNSquareRoot(){
-    int[] numbers = {6};
+    ArrayList<Double> numbers = new ArrayList<>(Arrays.asList(6d));
     RPNCalculatorRoom7 room = new RPNCalculatorRoom7();
     assertThat(room.calculateOperation(numbers, "SQRT"), equalTo(2.449489742783178));
   }
 
   @Test
   void testCalculateRPNMAX(){
-    int[] numbers = {6, 2, 1, 9, 13, 4};
+    ArrayList<Double> numbers = new ArrayList<>(Arrays.asList(6d, 2d, 1d, 9d, 13d, 4d));
     RPNCalculatorRoom7 room = new RPNCalculatorRoom7();
     assertThat(room.calculateOperation(numbers, "MAX"), equalTo(13.0));
   }
 
   @Test
   void testCalculateRPNDivide(){
-    int[] numbers = {11, 2};
+    ArrayList<Double> numbers = new ArrayList<>(Arrays.asList(11d, 2d));
     RPNCalculatorRoom7 room = new RPNCalculatorRoom7();
     assertThat(room.calculateOperation(numbers, "/"), equalTo(5.5));
   }
 
   @Test
   void testCalculateRPNMultiply(){
-    int[] numbers = {11, 2};
+    ArrayList<Double> numbers = new ArrayList<>(Arrays.asList(11d, 2d));
     RPNCalculatorRoom7 room = new RPNCalculatorRoom7();
     assertThat(room.calculateOperation(numbers, "*"), equalTo(22.0));
   }
 
   @Test
   void testCalculateRPNMinus(){
-    int[] numbers = {11, 2};
+    ArrayList<Double> numbers = new ArrayList<>(Arrays.asList(11d, 2d));
     RPNCalculatorRoom7 room = new RPNCalculatorRoom7();
     assertThat(room.calculateOperation(numbers, "-"), equalTo(9.0));
   }
 
   @Test
   void testCalculateRPNPlus(){
-    int[] numbers = {11, 2};
+    ArrayList<Double> numbers = new ArrayList<>(Arrays.asList(11d, 2d));
     RPNCalculatorRoom7 room = new RPNCalculatorRoom7();
     assertThat(room.calculateOperation(numbers, "+"), equalTo(13.0));
+  }
+
+  @Disabled
+  @Test
+  void testCommandLineArguments(){
+    String[] args = {"4", "2", "/", "3", "+", "SQRT", "5", "+"};
+    RPNCalculatorRoom7 room = new RPNCalculatorRoom7();
+    assertThat(room.calculateRPN(args), equalTo(5d));
   }
 }
