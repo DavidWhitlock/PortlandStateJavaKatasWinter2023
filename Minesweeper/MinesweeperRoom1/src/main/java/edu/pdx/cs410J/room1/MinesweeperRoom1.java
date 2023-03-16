fc272ec958503d1df327d2798b23a83fe8e46c56
SHREYOSHI MAHATO <shreyoshimahato@gmail.com>
2023-03-16 08:38:19 +0530

    the random matrix for game input

--- Minesweeper/MinesweeperRoom1/src/main/java/edu/pdx/cs410J/room1/MinesweeperRoom1.java
+++ Minesweeper/MinesweeperRoom1/src/main/java/edu/pdx/cs410J/room1/MinesweeperRoom1.java
@@ -2,12 +2,14 @@ package edu.pdx.cs410J.room1;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.Random;
+
 public class MinesweeperRoom1 {
 
   int m;
   int n;
 
-  MinesweeperRoom1(int m, int n){
+  MinesweeperRoom1(int m, int n) {
     this.m = m;
     this.n = n;
   }
@@ -17,12 +19,26 @@ public class MinesweeperRoom1 {
     System.err.println("Missing command line arguments");
   }
 
-  public void getMatrix() {
-    int[][] matrix = new int[m][n];
-    for(int i=0;i<m;i++){
-      for(int j=0;j<n;j++){
-        matrix[i][j] = 0;
+  @VisibleForTesting
+  public Character[][] getMatrix() {
+    Character[][] matrix = new Character[m][n];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        matrix[i][j] = '.';
       }
     }
+    return matrix;
+  }
+
+  @VisibleForTesting
+  public Character[][] getRandomMatrix() {
+    Character[][] matrix = getMatrix();
+    int k = 1 + (int) (Math.random() * m);// k is between 0 to m
+    for (int bombs = 1; bombs < k; bombs++) {
+      int i = (int) (Math.random() * m);
+      int j = (int) (Math.random() * n);
+      matrix[i][j] = '*';
+    }
+    return matrix;
   }
 }
--- Minesweeper/MinesweeperRoom1/src/test/java/edu/pdx/cs410J/room1/MinesweeperRoom1Test.java
+++ Minesweeper/MinesweeperRoom1/src/test/java/edu/pdx/cs410J/room1/MinesweeperRoom1Test.java
@@ -1,17 +1,19 @@
 package edu.pdx.cs410J.room1;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class MinesweeperRoom1Test {
 
 
   @Test
-  void createsMatrixCorrectly(){
-    int m = 4, n =3;
+  void createsMatrixCorrectly() {
+    int m = 4, n = 3;
     MinesweeperRoom1 game = new MinesweeperRoom1(m, n);
-    game.getMatrix();
-
-
+    Character[][] matrix = game.getMatrix();
+    MatcherAssert.assertThat(matrix[0][0], equalTo('.'));
   }
 
 
